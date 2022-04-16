19f22f3552d2baeda0754b788fec7dbd7812f7ea
StephanRhodes@DEKSTOP-UV6CR3J.(none) <amplifier339@comcast.net>
2022-04-16 01:47:04 -0700

    first commit

---
+++ src/components/MainComponent.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Directory from './DirectoryComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { CAMPSITES } from '../shared/campsites';
+
+class Main extends Component{
+  constructor(props){
+      super(props)
+      this.state = {
+        campsites: CAMPSITES
+      }
+  }
+
+  render(){
+      const HomePage = () => {
+          return (
+              <Home/>
+          )
+      }
+    return(
+        <div>
+          <Header/>
+          <Switch>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites}/>} />
+              <Redirect to='/home'/>
+          </Switch>
+          <Footer/>
+        </div>
+    );
+  }
+}
+
+export default Main;
