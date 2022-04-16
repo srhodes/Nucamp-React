558d61920b3dbec5ff75b49969e73e96f07eb541
StephanRhodes@DEKSTOP-UV6CR3J.(none) <amplifier339@comcast.net>
2022-04-16 03:58:23 -0700

    single page app

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,21 +4,32 @@ import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
 
 class Main extends Component{
   constructor(props){
       super(props)
       this.state = {
-        campsites: CAMPSITES
+        campsites: CAMPSITES,
+        comments: COMMENTS,
+        partners: PARTNERS,
+        promotions: PROMOTIONS
       }
   }
 
   render(){
       const HomePage = () => {
           return (
-              <Home/>
+              <Home
+                campsite={this.state.campsites.filter(campsites => campsites.featured)[0]}
+                promotion={this.state.promotions.filter(promotions => promotions.featured)[0]}
+                partner={this.state.partners.filter(partners => partners.featured)[0]}
+              />
           )
       }
     return(
@@ -28,6 +39,7 @@ class Main extends Component{
           <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites}/>} />
+              <Route exact path='/contactus' component={Contact}/>
               <Redirect to='/home'/>
           </Switch>
           <Footer/>
